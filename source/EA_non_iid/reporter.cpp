acba7e40337df363ec859d5bbc87275c180bca7b
genya <g.garland823@gmail.com>
2025-04-13 15:21:56 +0900

    modify

--- source/EA_non_iid/reporter.cpp
+++ source/EA_non_iid/reporter.cpp
@@ -1258,7 +1258,10 @@ ns_consts::EnmReturnStatus reportLaTeXSupportingCompilerInfo(std::wstringstream&
     o_refLaTeXSupportingInfo << L"\\, & built by & ";
     {
 #if defined(_MSC_VER) && !defined(__INTEL_LLVM_COMPILER) && !defined(__INTEL_COMPILER)
-#if _MSC_VER >= 1943
+#if _MSC_VER >= 1944
+        o_refLaTeXSupportingInfo << L" Visual Studio 2022 version 17.14";
+        o_refLaTeXSupportingInfo << L" (\\verb|_MSC_FULL_VER|: " << _MSC_FULL_VER << L" )";
+#elif _MSC_VER >= 1943
         o_refLaTeXSupportingInfo << L" Visual Studio 2022 version 17.13";
         o_refLaTeXSupportingInfo << L" (\\verb|_MSC_FULL_VER|: " << _MSC_FULL_VER << L" )";
 #elif _MSC_VER >= 1942
